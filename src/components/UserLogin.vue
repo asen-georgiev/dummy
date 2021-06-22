171d3855d515b62b1400b37857baf9862ff7579f
Asen Georgiev <asen.georgiev@devhost.bg>
2021-06-22 11:35:54 +0300

    Initial commit.

---
+++ src/components/UserLogin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>User Login</h1>
+    <b-form>
+      <b-form-group
+        id="email-group"
+        label="User email"
+        label-for="email-input"
+        description="We will always share your credentials">
+
+        <b-form-input
+          id="email-input"
+          v-model="user.userEmail"
+          type="email"
+          placeholder="Enter your email"
+          required>
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="password-group"
+        label="User password"
+        label-for="password-input"
+        description="Your password must be min. 8 characters long">
+
+        <b-form-input
+          id="password-input"
+          v-model="user.userPassword"
+          type="password"
+          placeholder="Enter your password"
+          required>
+        </b-form-input>
+      </b-form-group>
+
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLogin',
+  data () {
+    return {
+      user: {
+        userEmail: '',
+        userPassword: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
